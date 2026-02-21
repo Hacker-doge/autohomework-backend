db3804da02d906ce7462aea40fd9b1a3625556a9
krit <amporn.tv@gmail.com>
2026-02-21 16:32:27 +0700

    idk bro I just say yolo

--- src/main/kotlin/com/example/autohomework/PDF edit.kt
+++ src/main/kotlin/com/example/autohomework/PDF edit.kt
@@ -7,13 +7,8 @@ import ai.koog.agents.ext.tool.file.WriteFileTool
 import ai.koog.prompt.executor.clients.openai.OpenAIModels
 import ai.koog.prompt.executor.llms.all.simpleOpenAIExecutor
 import ai.koog.rag.base.files.JVMFileSystemProvider
-import io.github.cdimascio.dotenv.Dotenv
-import io.github.cdimascio.dotenv.dotenv
 
-class GPTCilent()  {
-    val dotenv = dotenv()
-    val tokem = dotenv["TOK_KEY"]
-
+class GPTCilent(tokem: String)  {
 
     val toolRegistry = ToolRegistry {
         tool(ReadFileTool(JVMFileSystemProvider.ReadOnly))
@@ -21,7 +16,7 @@ class GPTCilent()  {
     }
 
     val agent = AIAgentService(
-        promptExecutor = simpleOpenAIExecutor(tokem.toString()),
+        promptExecutor = simpleOpenAIExecutor(tokem),
         systemPrompt = "You do the pdf that I gave you",
         llmModel = OpenAIModels.Chat.GPT5Mini,
         toolRegistry = toolRegistry,
